7ad8e1249ebfcb43c35610c00b62e5bb4e2b980d
Blake Weichbrodt <blakeweichbrodt7@gmail.com>
2025-10-14 18:43:54 -0500

    Finished Fetch

--- rtl/fetch.v
+++ rtl/fetch.v
@@ -1,20 +1,30 @@
-module(
-    input clk, rst_n,
+module fetch(
+    input clk, i_rst,
     input [31:0] new_pc,
     input [31:0] i_imem_rdata,
-    output [31:0] read_addr,
-    output [31:0] instruction
-    output [31:0] next_pc;
-    output reg [31:0] pc;
-
+    input [31:0] reset_addr,
+    output [31:0] o_imem_raddr,
+    output [31:0] instruction,
+    output [31:0] pc_plus4,
+    output reg [31:0] pc
 );
 
-    always @(posedge clk, negedge rst_n) begin
+    // Infers PC flop with active high reset
+    always @(posedge clk) begin
         // infer pc reg
+        if (i_rst)
+            pc <= reset_addr;
+        else
+            pc <= new_pc;
     end
 
+    // increment pc by 4
+    assign pc_plus4 = pc + 32'h4;
+
+    // Output the instruction read from memory
     assign instruction = i_imem_rdata;
 
-
+    // set o_imem_raddr to current pc (so it can be used to fetch instruction in hart.v)
+    assign o_imem_raddr = pc;
 
 endmodule
