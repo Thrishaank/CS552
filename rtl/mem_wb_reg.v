de03effd442912e1ff0e9bacc1816f613d089de8
Blake Weichbrodt <blakeweichbrodt7@gmail.com>
2025-11-01 17:41:47 -0500

    Modified writeback and added mem_wb_reg for pipelining stage

---
+++ rtl/mem_wb_reg.v
@@ -0,0 +1,41 @@
+module mem_wb_reg(
+    input i_clk, i_rst, i_stall,
+    // Inputs
+    input i_mem_read, i_reg_write,
+    input [31:0] i_mem_data_out, i_alu_result,
+    // Outputs
+    input o_em_read, o_reg_write,
+    input [31:0] o_mem_data_out, o_alu_result,
+);
+
+wire [31:0] NOP = 32'h00000013;
+
+d_ff mem_read_ff (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .d(i_stall ? o_mem_read : i_mem_read),
+    .q(o_mem_read)
+);
+
+d_ff reg_write_ff (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .d(i_stall ? o_reg_write : i_reg_write),
+    .q(o_reg_write)
+);
+
+d_ff #(.WIDTH(32), .RST_VAL(NOP)) mem_data_out_ff (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .d(i_stall ? o_mem_data_out : i_mem_data_out),
+    .q(o_mem_data_out)
+);
+
+d_ff #(.WIDTH(32), .RST_VAL(NOP)) alu_result_ff (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .d(i_stall ? o_alu_result : i_alu_result),
+    .q(o_alu_result)
+);
+
+endmodule
--- rtl/writeback.v
+++ rtl/writeback.v
@@ -1,12 +1,16 @@
 module writeback(
     input i_clk, i_rst,
-    input mem_read, reg_write, is_auipc, is_lui, jump,
-    input [31:0] mem_data_out, alu_result, pc, pc_plus4, imm,
+    input mem_read, reg_write,
+    input [31:0] mem_data_out, alu_result,
     output [31:0] reg_write_data,
     output reg_write_en
 );
     // TODO: remove LUI and AUIPC from mux, will just get from execute output
 
+    assign reg_write_data = mem_read ? mem_data_out : alu_result;
+    assign reg_write_en = reg_write;
+
+    /* KEPT IF NEEDED IN FUTURE
     assign reg_write_data = jump
         ? pc_plus4 // jal or jalr
         : (is_lui)
@@ -16,8 +20,6 @@ module writeback(
                 : mem_read 
                     ? mem_data_out // load
                     : alu_result; // All other instructions
-
-    // Will be useful when pipelining
-    assign reg_write_en = reg_write;
+    */
 
 endmodule
